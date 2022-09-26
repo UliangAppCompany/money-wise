f2a0936031782b14be902d3c15ebb29a53dd5a92
Tang U-Liang <uliang6482@gmail.com>
2022-09-27 07:27:20 +0800

    Modified account_management tests.py
    Corrected tests because model objects have changed.

--- app/account_management/tests.py
+++ app/account_management/tests.py
@@ -7,10 +7,7 @@ from .models import *
 
 @pytest.mark.django_db
 def test_initialize_objects_in_database():
-    asset = AccountType.objects.create(name="AS")
-
-    current_asset = AccountCategory.objects.create(name="Current Asset", taip=asset)
-
-    cash_account = Account(name="Cash", category=current_asset)
+    current_asset = AccountCategory.objects.create(name="Current Asset", group="AS", )
+    cash_account = Account(number=1, category=current_asset)
 
     cash_account.save()
