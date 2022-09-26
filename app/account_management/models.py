fd71b8b917a697bbe35a7e9c458096e436f2b217
Tang U-Liang <uliang6482@gmail.com>
2022-09-27 07:23:35 +0800

    Modified account_management models.py
    AccountCategories can now support heirachical categorization.
    Account Type is now renamed to group and it is a choice field
    on the AccountCategories object.

--- app/account_management/models.py
+++ app/account_management/models.py
@@ -5,30 +5,30 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Account(models.Model):
-    name = models.CharField(max_length=30)
+    number = models.IntegerField()
     description = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
-    debit_balance = models.BooleanField(default=True)
-    balance = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    should_debit_balance = models.BooleanField(default=True)
+    credit_balance = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    debit_balance = models.DecimalField(max_digits=12, decimal_places=2, default=0)
     category = models.ForeignKey(
         "AccountCategory", on_delete=models.CASCADE, related_name="accounts"
     )
+
+    @property
+    def balance(self):
+        return (
+            self.debit_balance - self.credit_balance
+            if self.should_debit_balance
+            else self.credit_balance - self.debit_balance
+        )
 
 
 class AccountCategory(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(blank=True)
-
-    taip = models.ForeignKey(
-        "AccountType",
-        on_delete=models.CASCADE,
-        related_name="categories",
-    )
-
-
-class AccountType(models.Model):
-    name = models.CharField(
+    group = models.CharField(
         max_length=2,
         choices=[
             ("AS", _("Assets")),
@@ -36,8 +36,11 @@ class AccountType(models.Model):
             ("CA", _("Capital")),
             ("RV", _("Revenue")),
             ("EX", _("Expense")),
-        ],
+        ] 
+    )
+    supercategory = models.ForeignKey(
+        "self", on_delete=models.CASCADE, related_name="subcategories",null=True 
     )
 
     def get_all_accounts(self):
-        return Account.objects.filter(category__taip=self)
+        return Account.objects.filter(category=self)
