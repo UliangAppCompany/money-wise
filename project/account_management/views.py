d4cf90c8e34b7667d78627e725575e2b463ccf47
Tang U-Liang <uliang6482@gmail.com>
2022-09-29 07:38:25 +0800

    UL-10 Update app.urls.py and implement post category view
    Users can now create new categories and associate it with a
    parent category via an api call.

--- project/account_management/views.py
+++ project/account_management/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+import ninja
+from ninja import NinjaAPI 
+from account_management.models  import AccountCategory
 
 # Create your views here.
+
+
+api = NinjaAPI() 
+
+class AccountCategoryResponseSchema(ninja.ModelSchema):
+    supercategory: 'AccountCategoryResponseSchema' = None 
+    class Config: 
+        model=AccountCategory
+        model_fields = ['id','name', 'description']
+
+
+AccountCategoryResponseSchema.update_forward_refs()
+
+class AccountCategoryRequestSchema(ninja.ModelSchema): 
+    supercategory: int 
+    class Config: 
+        model=AccountCategory
+        model_fields = ['name', 'description']
+
+
+@api.post('/account-management/account-category', response={201: AccountCategoryResponseSchema})
+def post_new_category(request, data:AccountCategoryRequestSchema): 
+    parent_cat = AccountCategory.objects.get(id=data.supercategory)
+    cat = AccountCategory(name=data.name, description=data.description)
+    cat.supercategory = parent_cat
+    cat.save()
+    return 201, cat 
