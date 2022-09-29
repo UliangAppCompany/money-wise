4af13652a322e2a19f26daca9ded4e99e90f0697
uliang <uliang6482@gmail.com>
2022-09-29 14:56:21 +0800

    UL-10 Reconfigure urls

--- project/account_management/urls.py
+++ project/account_management/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 import account_management.views.account_category 
 
 urlpatterns = [
-    path('', account_management.views.account_category.api.urls)
+    path('account-management/', account_management.views.account_category.api.urls)
 ]
--- project/app/urls.py
+++ project/app/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('api/account-management/', include('account_management.urls'))
+    path('api/', include('account_management.urls')), 
 ]
