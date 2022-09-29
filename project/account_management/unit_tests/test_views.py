6cf0cbb9f96dc5a7c0b80f069470b30ef64697ec
Tang U-Liang <uliang6482@gmail.com>
2022-09-29 19:24:06 +0800

    UL-10 Implement get account category endpoint
    We now can get list of account categories from the database.

--- project/account_management/unit_tests/test_views.py
+++ project/account_management/unit_tests/test_views.py
@@ -1,108 +1,145 @@
-from account_management.views import SupercategoryUnavailableError
-from account_management.models import AccountCategory
 import pytest
 
-@pytest.fixture 
+
+@pytest.fixture
 def setup_initial_category(cursor):
-    cursor.execute('''
+    cursor.execute(
+        """
             insert into account_management_accountcategory 
             (id, name, supercategory_id) 
             values 
             (1, 'Assets', null); 
-            ''')
+            """
+    )
 
 
 @pytest.mark.django_db
-@pytest.mark.usefixtures('setup_initial_category')
-def test_post_new_category_returns_created_response(client): 
+@pytest.mark.usefixtures("setup_initial_category")
+def test_post_new_category_returns_created_response(client):
 
-    response = client.post('/api/account-management/account-category', {
-            'name': 'Cash', 
-            'description': 'All liquid assets stored in bank or petty cash', 
-            'supercategory': 1
-        }, content_type='application/json') 
+    response = client.post(
+        "/api/account-management/account-category",
+        {
+            "name": "Cash",
+            "description": "All liquid assets stored in bank or petty cash",
+            "supercategory": 1,
+        },
+        content_type="application/json",
+    )
 
-    assert response.status_code == 201 
+    assert response.status_code == 201
     assert response.json() == {
-            'id': 2, 
-            'name': 'Cash', 
-            'description': 'All liquid assets stored in bank or petty cash', 
-            'supercategory': {
-                'id': 1, 
-                'name': 'Assets', 
-                'description': None, 
-                'supercategory': None
-                } 
-             }
-
-@pytest.mark.django_db
-@pytest.mark.usefixtures('setup_initial_category')
-def test_after_post_database_is_updated(cursor, client): 
-    client.post('/api/account-management/account-category', {
-            'name': 'Cash', 
-            'description': 'All liquid assets stored in bank or petty cash', 
-            'supercategory': 1
-        }, content_type='application/json') 
-
-    cursor.execute('select * from account_management_accountcategory') 
-    assert len(cursor.fetchall()) == 2 
-    
-
-@pytest.mark.django_db
-@pytest.mark.usefixtures('setup_initial_category')
-def test_relationship_between_category_is_reflected_in_the_model(client, cursor): 
-    
-    client.post('/api/account-management/account-category', {
-            'name': 'Cash', 
-            'description': 'All liquid assets stored in bank or petty cash', 
-            'supercategory': 1
-        }, content_type='application/json') 
-
-    client.post('/api/account-management/account-category', {
-            'name': 'Accounts Receivable' ,
-            'description': 'Vendors that owe us money for goods sold.',
-            'supercategory': 1
-        }, content_type='application/json') 
-
-    cursor.execute("""
-        select name from account_management_accountcategory
-        where supercategory_id = 1 
-        """)
-    assert cursor.fetchall() == [('Cash', ), ('Accounts Receivable',)]
-
-
-@pytest.mark.django_db 
-@pytest.mark.usefixtures("setup_initial_category") 
-def test_post_base_account_category(client): 
-    response = client.post('/api/account-management/account-category', {
-        'name': 'Liabilities', 
-        'description': 'Liability accounts', 
-        'supercategory': None
-    }, content_type='application/json')  
-
-    assert response.status_code == 201 
-    assert response.json() == { 
-        'id':2, 
-        'name': 'Liabilities', 
-        'description': 'Liability accounts', 
-        'supercategory': None
+        "id": 2,
+        "name": "Cash",
+        "description": "All liquid assets stored in bank or petty cash",
+        "supercategory": {
+            "id": 1,
+            "name": "Assets",
+            "description": None,
+            "supercategory": None,
+        },
     }
 
 
-@pytest.mark.django_db 
-@pytest.mark.usefixtures("setup_initial_category") 
-def test_400_error_is_raised_when_parent_category_is_not_detected(client): 
-    response = client.post('/api/account-management/account-category', {
-            'name': 'Liabilities', 
-            'description': 'Liability accounts', 
-            'supercategory': 0
-    }, content_type='application/json')  
+@pytest.mark.django_db
+@pytest.mark.usefixtures("setup_initial_category")
+def test_after_post_database_is_updated(cursor, client):
+    client.post(
+        "/api/account-management/account-category",
+        {
+            "name": "Cash",
+            "description": "All liquid assets stored in bank or petty cash",
+            "supercategory": 1,
+        },
+        content_type="application/json",
+    )
 
-    assert response.status_code == 400 
-    assert response.json()['message'] == "Parent category not found."
+    cursor.execute("select * from account_management_accountcategory")
+    assert len(cursor.fetchall()) == 2
 
 
-def test_documentation_page_loads_from_the_correct_url(client): 
-    response = client.get('/api/account-management/docs') 
+@pytest.mark.django_db
+@pytest.mark.usefixtures("setup_initial_category")
+def test_relationship_between_category_is_reflected_in_the_model(client, cursor):
 
-    assert response.status_code == 200 
+    client.post(
+        "/api/account-management/account-category",
+        {
+            "name": "Cash",
+            "description": "All liquid assets stored in bank or petty cash",
+            "supercategory": 1,
+        },
+        content_type="application/json",
+    )
+
+    client.post(
+        "/api/account-management/account-category",
+        {
+            "name": "Accounts Receivable",
+            "description": "Vendors that owe us money for goods sold.",
+            "supercategory": 1,
+        },
+        content_type="application/json",
+    )
+
+    cursor.execute(
+        """
+        select name from account_management_accountcategory
+        where supercategory_id = 1 
+        """
+    )
+    assert cursor.fetchall() == [("Cash",), ("Accounts Receivable",)]
+
+
+@pytest.mark.django_db
+@pytest.mark.usefixtures("setup_initial_category")
+def test_post_base_account_category(client):
+    response = client.post(
+        "/api/account-management/account-category",
+        {
+            "name": "Liabilities",
+            "description": "Liability accounts",
+            "supercategory": None,
+        },
+        content_type="application/json",
+    )
+
+    assert response.status_code == 201
+    assert response.json() == {
+        "id": 2,
+        "name": "Liabilities",
+        "description": "Liability accounts",
+        "supercategory": None,
+    }
+
+
+@pytest.mark.django_db
+@pytest.mark.usefixtures("setup_initial_category")
+def test_400_error_is_raised_when_parent_category_is_not_detected(client):
+    response = client.post(
+        "/api/account-management/account-category",
+        {
+            "name": "Liabilities",
+            "description": "Liability accounts",
+            "supercategory": 0,
+        },
+        content_type="application/json",
+    )
+
+    assert response.status_code == 400
+    assert response.json()["message"] == "Parent category not found."
+
+
+def test_documentation_page_loads_from_the_correct_url(client):
+    response = client.get("/api/account-management/docs")
+
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+@pytest.mark.usefixtures("setup_fake_accounts")
+def test_list_of_account_categories(client):
+    response = client.get("/api/account-management/account-category")
+
+    assert response.status_code == 200
+    assert len(response.json()) == 18
--- project/account_management/views/account_category.py
+++ project/account_management/views/account_category.py
@@ -1,37 +1,46 @@
 from typing import Optional
 import ninja
-# from ninja import NinjaAPI 
-from account_management.models  import AccountCategory
+
+# from ninja import NinjaAPI
+from account_management.models import AccountCategory
 from account_management.views import SupercategoryUnavailableError, api
 
 # Create your views here.
 
 
 class AccountCategoryResponseSchema(ninja.ModelSchema):
-    supercategory: 'AccountCategoryResponseSchema' = None 
-    class Config: 
-        model=AccountCategory
-        model_fields = ['id','name', 'description']
+    supercategory: "AccountCategoryResponseSchema" = None
+
+    class Config:
+        model = AccountCategory
+        model_fields = ["id", "name", "description"]
 
 
 AccountCategoryResponseSchema.update_forward_refs()
 
-class AccountCategoryRequestSchema(ninja.ModelSchema): 
-    supercategory: Optional[int] 
-    class Config: 
-        model=AccountCategory
-        model_fields = ['name', 'description']
+
+class AccountCategoryRequestSchema(ninja.ModelSchema):
+    supercategory: Optional[int]
+
+    class Config:
+        model = AccountCategory
+        model_fields = ["name", "description"]
 
 
-@api.post('/account-category', response={201: AccountCategoryResponseSchema})
-def post_new_category(request, data:AccountCategoryRequestSchema): 
+@api.get("/account-category", response={200: list[AccountCategoryResponseSchema]})
+def get_account_categories(request):
+    cats = AccountCategory.objects.all()
+    return 200, cats
+
+
+@api.post("/account-category", response={201: AccountCategoryResponseSchema})
+def post_new_category(request, data: AccountCategoryRequestSchema):
     cat = AccountCategory(name=data.name, description=data.description)
     if data.supercategory is not None:
-        try:  
+        try:
             parent_cat = AccountCategory.objects.get(id=data.supercategory)
-        except AccountCategory.DoesNotExist: 
+        except AccountCategory.DoesNotExist:
             raise SupercategoryUnavailableError()
         cat.supercategory = parent_cat
     cat.save()
-    return 201, cat 
-
+    return 201, cat
