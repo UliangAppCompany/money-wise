8995c1a688146ea218973021a5e074c287f030ac
Tang U-Liang <uliang6482@gmail.com>
2022-10-19 21:15:17 +0800

    UL-7 Updated registration test_service module
    Added test to check that email is sent when new user is saved to
    database.

--- project/registration/unit_tests/test_service.py
+++ project/registration/unit_tests/test_service.py
@@ -1,6 +1,9 @@
+import pytest 
+
+from django.core import mail 
+
 from registration.exceptions import DuplicateUserNameError
 from registration.service import create_user 
-import pytest 
 
 
 
@@ -18,3 +21,13 @@ def test_that_new_user_cannot_be_created_if_already_present():
         create_user('john@example.com', 'p@s$w0rd')
     
         assert str(exc) == "User john@example.com already registered."
+
+@pytest.mark.django_db 
+@pytest.mark.usefixtures("register_new_user")
+def test_that_validation_email_is_sent_when_new_user_is_successfully_saved_in_db(): 
+    assert len(mail.outbox) == 1    
+    message = mail.outbox[0] 
+
+    assert message.subject == "New user validation link" 
+    assert message.from_email == "admin@money-wise.com.my"
+    assert message.to == ["john@example.com"]
