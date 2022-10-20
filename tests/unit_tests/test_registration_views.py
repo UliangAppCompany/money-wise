1ff8795f957d81e7c6b6571a9eae0ee734cc4e79
Tang U-Liang <uliang6482@gmail.com>
2022-10-20 21:45:23 +0800

    ul-7 Updating unit test for registration view

--- tests/unit_tests/test_registration_views.py
+++ tests/unit_tests/test_registration_views.py
@@ -1,5 +1,8 @@
+import pytest 
 
+@pytest.mark.django_db
+@pytest.mark.usefixtures("register_new_user")
 def test_validation_url(client): 
-    response = client.get('/registration/validate?token=abc')
+    response = client.get('/registration/user/1/validate?token=abc')
 
     assert response.status_code == 302
