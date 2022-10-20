cbaec69408744b35440e71f99d5ab2bd85b0fb2a
Tang U-Liang <uliang6482@gmail.com>
2022-10-20 18:59:34 +0800

    ul-7 Changed project directory structure
    Moved registration unit tests into central testing package

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,12 @@ from django.test import Client
 
 
 from account_management.models import Journal, Ledger
+from registration.service import create_user
 
+
+@pytest.fixture 
+def register_new_user(): 
+    create_user('john@example.com', 'password') 
 
 @pytest.fixture 
 def cursor(): 
