bb963cc57d543717e885f42906c51f93aa671cc1
uliang <uliang6482@gmail.com>
2022-10-22 21:38:51 +0800

    ul-7 Implementing validation flow
    Validation tokens now are generated using the secrets module.
    Validation link has an expiry date.

--- project/registration/signals.py
+++ project/registration/signals.py
@@ -1,8 +1,10 @@
+import urllib.parse
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model 
-from django.conf import settings
 from django.core.mail import send_mail 
+from django.conf import settings
 
 
 @receiver(post_save, sender=get_user_model()) 
@@ -11,8 +13,15 @@ def send_message(sender, **kwargs):
     if kwargs['created']: 
         user_email_field = user.get_email_field_name() 
         user_email = getattr(user, user_email_field) 
+
         subject = "New user validation link" 
-        message = f'Please click here to validate your email: https://example.com/registration/user/{user.id}/validate?token={user.validation_token}'
+        validation_token = user.generate_validation_token() if user.validation_token is None else user.validation_token
+        validation_url = urllib.parse.urljoin(settings.HOSTNAME,
+            f'/registration/validate?username={user.username}&token={validation_token}')
+        message = f'Please click here to validate your email:{validation_url}' 
+
+        user.save()
+        
         send_mail(subject, message, from_email=None, recipient_list=[user_email])
 
         
--- project/registration/urls.py
+++ project/registration/urls.py
@@ -4,7 +4,7 @@ from registration import views
 
 
 urlpatterns = [
-        path('user/<int:user_id>/validate', views.validate_registration , name='validate-registration'), 
+        path('validate', views.validate_registration , name='validate-registration'), 
         path('set-new-password', views.set_new_password, name='set-new-password')
         ]
 
