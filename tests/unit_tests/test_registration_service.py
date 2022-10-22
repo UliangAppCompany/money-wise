612f9cf25683ae5238468830bce4619a53e872a4
uliang <uliang6482@gmail.com>
2022-10-22 20:45:05 +0800

    ul-7 Refactored unit test for validation flow
    Username identified is encoded in the validation link url. Added tests
    for expiry of validation link.

--- tests/unit_tests/test_registration_service.py
+++ tests/unit_tests/test_registration_service.py
@@ -1,7 +1,10 @@
+import time
+from datetime import timedelta
 import pytest 
 
 from django.core import mail 
 from django.contrib.auth import get_user_model 
+from django.test import override_settings
 
 from registration.exceptions import DuplicateUserNameError, UnvalidatedUserError
 from registration.service import create_user 
@@ -18,7 +21,7 @@ def test_create_new_user_service_creates_new_user_if_not_present(cursor):
 @pytest.mark.usefixtures("register_new_user")
 def test_that_new_user_cannot_be_created_if_already_present(): 
     with pytest.raises(DuplicateUserNameError) as exc: 
-        create_user('john@example.com', 'p@s$w0rd')
+        create_user('john@example.com')
     
         assert str(exc) == "User john@example.com already registered."
 
@@ -31,7 +34,7 @@ def test_that_validation_email_is_sent_when_new_user_is_successfully_saved_in_db
     assert message.subject == "New user validation link" 
     assert message.from_email == "admin@money-wise.com.my"
     assert message.to == ["john@example.com"]
-    assert 'registration/user/1/validate?token=abc' in message.body 
+    assert 'registration/validate?username=john@example.com&token=abc' in message.body 
 
 @pytest.mark.django_db 
 @pytest.mark.usefixtures("register_new_user") 
@@ -48,7 +51,7 @@ def test_that_email_is_not_sent_when_user_info_is_updated():
 @pytest.mark.django_db 
 @pytest.mark.usefixtures("register_new_user") 
 def test_that_clicked_validation_link_validates_user(client): 
-    response =client.get('/registration/user/1/validate?token=abc')
+    response =client.get('/registration/validate?username=john@example.com&token=abc')
     user = get_user_model().objects.get(username='john@example.com')
     
     assert user.is_validated == True
@@ -63,6 +66,24 @@ def test_that_unvalidated_user_cannot_set_password():
         user.set_password('password') 
         assert str(exc) == "Password cannot be set on unvalidated users."
 
+@pytest.mark.django_db
+@pytest.mark.usefixtures("register_new_user") 
+@override_settings(
+    VALIDATION_TOKEN_EXPIRY = timedelta(seconds=0.5)
+)
+def test_that_user_cannot_be_validated_if_validation_link_has_expired(client): 
+    time.sleep(0.6) 
+    response =client.get('/registration/validate?username=john@example.com&token=abc')
+    
+    user = get_user_model().objects.get(username='john@example.com')
+    assert user.is_validated==False
+    
+@pytest.mark.django_db
+@pytest.mark.usefixtures("register_new_user") 
+def test_send_message_callback_sends_message_with_urlsafe_token_if_none_supplied(): 
+    validation_token = get_user_model().objects.get(username='john@example.com').validation_token 
+    body = mail.outbox[0].body 
+    assert validation_token in body 
 
 @pytest.mark.django_db 
 @pytest.mark.usefixtures("register_new_user") 
