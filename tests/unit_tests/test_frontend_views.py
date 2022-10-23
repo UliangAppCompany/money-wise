fc839fd32e0901169eb2eb842698bdcac8fad679
uliang <uliang6482@gmail.com>
2022-10-23 11:07:48 +0800

    ul-9 Updated test_frontend_views.py
    Test that template renders with the correct forms and entities.

--- tests/unit_tests/test_frontend_views.py
+++ tests/unit_tests/test_frontend_views.py
@@ -21,3 +21,15 @@ def test_add_account_page_loads(client):
     response = client.get('/ledger/1/account')
 
     assert response.status_code == 200 
+
+@pytest.mark.django_db 
+@pytest.mark.usefixtures("register_new_user", "set_up_validated_user")
+def test_template_renders_with_correct_context(client): 
+
+    client.login(username='john@example.com', password='password')
+    response = client.get('/ledger/1/account') 
+    context = response.context 
+
+    assert isinstance(context['add_account_form'], AccountManagementAddAccountForm)  
+    assert isinstance(context['ledger'], Ledger)
+    assert 'frontend/account_management.html' in [template.name for template in response.templates]
