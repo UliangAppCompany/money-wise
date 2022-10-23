042f6646b30640c4ca736476f1418b974dabc560
uliang <uliang6482@gmail.com>
2022-10-23 11:01:29 +0800

    ul-9 Changed add account response unit test
    We intend to change the url for adding accounts. Test that the url
    resolves correctly

--- tests/unit_tests/test_frontend_views.py
+++ tests/unit_tests/test_frontend_views.py
@@ -1,6 +1,23 @@
-from urllib import response
+import pytest
+
+from django.contrib.auth import get_user_model
+
+from frontend.forms import AccountManagementAddAccountForm 
+from account_management.models import Ledger
 
 
+@pytest.fixture 
+def set_up_validated_user(ledger): 
+    user = get_user_model().objects.get(username='john@example.com')
+    user.is_validated=True
+    user.set_password('password') 
+    user.ledgers.add(ledger)
+    user.save() 
+
+@pytest.mark.django_db 
+@pytest.mark.usefixtures("register_new_user", "set_up_validated_user")
 def test_add_account_page_loads(client): 
-    response = client.get('/user/1/account')
+    client.login(username='john@example.com', password='password')
+    response = client.get('/ledger/1/account')
+
     assert response.status_code == 200 
