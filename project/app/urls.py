b5403c2cdbfd2f9cf7ca780561cf3eedb3d0bba0
uliang <uliang6482@gmail.com>
2022-10-23 11:41:47 +0800

    Updated app settings.py and urls.py
    Added ApiConfig class to INSTALLED_APPS and defined routes for
    api endpoints.

--- project/app/urls.py
+++ project/app/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path 
+from django.urls import path
 
 import account_management.views
+import api.views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", account_management.views.api.urls),
+    path("api/", api.views.api.urls),
 ]
