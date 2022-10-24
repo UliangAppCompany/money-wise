ff62dca062ab5d2c0ff9b7b26158aa643b58ad4a
uliang <uliang6482@gmail.com>
2022-10-24 22:50:28 +0800

    ul-9 Updated api views.py
    Added view function to handle post request for adding accounts to server

--- project/api/views.py
+++ project/api/views.py
@@ -4,6 +4,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
-api = NinjaAPI()
+api = NinjaAPI(version='1')
+
+@api.post("account", url_name='add-new-account')
+def add_account(request): 
+    ...
 
 
