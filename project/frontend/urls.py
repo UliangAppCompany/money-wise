8d6fba5a0c613f501b3f0942912ff2371f0c2df9
uliang <uliang6482@gmail.com>
2022-10-24 22:49:40 +0800

    ul-9 Updated frontend urls.py
    We updated the routing for adding new accounts

--- project/frontend/urls.py
+++ project/frontend/urls.py
@@ -4,5 +4,6 @@ from frontend import views
 
 
 urlpatterns = [
-    path('ledger/<int:ledger_id>/account', views.account_management, name='account-management')
+    path('ledger/<int:ledger_id>/account/new', views.add_new_account, name='add-new-account'), 
+    path('ledger/<int:ledger_id>/account', views.list_accounts, name='list-accounts')
 ]
