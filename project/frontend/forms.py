3d96f0ace976c1de9dcad72ce9e6a1f9245b74c1
uliang <uliang6482@gmail.com>
2022-10-24 22:47:40 +0800

    ul-9 Implementing the view and frontend
    We implement the forms in HTML and submit action using jQuery.

--- project/frontend/forms.py
+++ project/frontend/forms.py
@@ -4,8 +4,6 @@ from account_management.models import Account
 class AccountManagementAddAccountForm(forms.ModelForm):
     class Meta: 
         model = Account
-        fields = ['number', 'description', 
-                'debit_account', 'category',  
-                'is_control']
+        fields = ['number', 'description', 'debit_account'] 
                 
         
--- project/frontend/views.py
+++ project/frontend/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
 from django.contrib.auth import get_user
+from django.urls import reverse_lazy
+from django.contrib.auth.models import AnonymousUser
 
 from .forms import AccountManagementAddAccountForm
 
 # Create your views here.
-def account_management(request, ledger_id): 
+def add_new_account(request, ledger_id): 
     add_account_form =  AccountManagementAddAccountForm()
     user = get_user(request)
-    ledger = user.ledgers.get(id=ledger_id)
-    return render(request, 'frontend/account_management.html',
+    ledger = None
+    if not isinstance(user, AnonymousUser): 
+        ledger = user.ledgers.get(id=ledger_id)
+    add_account_url = reverse_lazy("api-1:add-new-account")
+    return render(request, 'frontend/add_new_account.html',
        context={
-            'add_account_form': add_account_form, 
-            'ledger':ledger }
+            'form': add_account_form, 
+            'ledger':ledger,  
+            'post_url': add_account_url}
         )
+
+def list_accounts(request): 
+    ...
