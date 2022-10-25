4b93f09605eedb72c82bc41b438f0641999a0d71
Tang U-Liang <uliang6482@gmail.com>
2022-10-26 07:26:33 +0800

    ul-9 Updated frontend views
    Used reverse function instead of reverse lazy in add account view
    and passed in ledger_page url to the template context.

--- project/frontend/views.py
+++ project/frontend/views.py
@@ -2,7 +2,7 @@ import re
 from typing import List
 from django.shortcuts import render
 from django.contrib.auth import get_user
-from django.urls import reverse_lazy
+from django.urls import reverse
 
 from .forms import AccountManagementAddAccountForm
 
@@ -11,12 +11,14 @@ def add_account(request, ledger_id):
     add_account_form =  AccountManagementAddAccountForm()
     user = get_user(request)
     ledger = user.ledgers.get(id=ledger_id)
-    add_account_url = reverse_lazy("api-1:add-new-account")
+    add_account_url = reverse("api-1:add-new-account")
+    ledger_page = reverse("ledger-page", args=[ledger_id])
     return render(request, 'frontend/add_account_page.html',
        context={
             'form': add_account_form, 
             'ledger':ledger,  
-            'post_url': add_account_url}
+            'post_url': add_account_url, 
+            'ledger_page': ledger_page}
         )
 
 def ledger_management(request, ledger_id): 
