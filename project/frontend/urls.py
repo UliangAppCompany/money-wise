9bf73dfa62ad8d2098b58129e3aa741eb669e659
uliang <uliang6482@gmail.com>
2022-10-25 17:43:13 +0800

    ul-9 Updating routing on add account view function
    The naming of the route is made more consistent with the entity
    structure of the application. Ledger containing accounts. So
    by ending the route with accounts, and with it being a post request
    already suggests we want to add an account to ledger.

--- project/frontend/urls.py
+++ project/frontend/urls.py
@@ -4,6 +4,6 @@ from frontend import views
 
 
 urlpatterns = [
-    path('ledger/<int:ledger_id>/account/new', views.add_new_account, name='add-new-account'), 
+    path('ledger/<int:ledger_id>/account', views.add_account, name='add-account'), 
     path('ledger/<int:ledger_id>/account', views.list_accounts, name='list-accounts')
 ]
--- project/frontend/views.py
+++ project/frontend/views.py
@@ -1,19 +1,18 @@
+import re
+from typing import List
 from django.shortcuts import render
 from django.contrib.auth import get_user
 from django.urls import reverse_lazy
-from django.contrib.auth.models import AnonymousUser
 
 from .forms import AccountManagementAddAccountForm
 
 # Create your views here.
-def add_new_account(request, ledger_id): 
+def add_account(request, ledger_id): 
     add_account_form =  AccountManagementAddAccountForm()
     user = get_user(request)
-    ledger = None
-    if not isinstance(user, AnonymousUser): 
-        ledger = user.ledgers.get(id=ledger_id)
+    ledger = user.ledgers.get(id=ledger_id)
     add_account_url = reverse_lazy("api-1:add-new-account")
-    return render(request, 'frontend/add_new_account.html',
+    return render(request, 'frontend/add_account_page.html',
        context={
             'form': add_account_form, 
             'ledger':ledger,  
