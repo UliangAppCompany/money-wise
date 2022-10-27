79eda35df4db43e1caf5c8de663c1ffd4dfca13c
uliang <uliang6482@gmail.com>
2022-10-27 09:55:24 +0800

    ul-9 Update api/schemas.py
    The schemas are updated in response to changes in the the add account
    form changes.

--- project/api/schemas.py
+++ project/api/schemas.py
@@ -6,10 +6,11 @@ from account_management.models import Account
 class AccountSchema(ModelSchema): 
     class Config:
         model = Account 
-        model_fields = ['number', 'description', 'debit_account']
+        model_fields = ['number', 'name', 'description', 'notes', 'debit_account']
 
 class AccountResponseSchema(ModelSchema): 
     class Config:
         model = Account 
-        model_fields = ['id', 'number', 'description', 'debit_account']
+        model_fields = ['id', 'number', 'name', 'description', 'notes', 
+                        'debit_account']
 
