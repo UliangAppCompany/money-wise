f2c234b7d7bb8282f749a7057e828ee36e4e296b
uliang <uliang6482@gmail.com>
2022-10-27 09:56:21 +0800

    ul-9 Updated forms
    Added new fields to the form

--- project/frontend/forms.py
+++ project/frontend/forms.py
@@ -4,6 +4,6 @@ from account_management.models import Account
 class AccountManagementAddAccountForm(forms.ModelForm):
     class Meta: 
         model = Account
-        fields = ['number', 'description', 'debit_account'] 
+        fields = ['number', 'name', 'description', 'notes', 'debit_account'] 
                 
         
