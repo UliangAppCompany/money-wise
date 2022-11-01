090fc2184cb6c0b2c217226bec14892bc9b1ec64
uliang <uliang6482@gmail.com>
2022-11-01 15:11:16 +0800

    ul-20 Updated api schemas.py
    Added LedgerSchema and LedgerResponseSchema

--- project/api/schemas.py
+++ project/api/schemas.py
@@ -2,6 +2,8 @@ from ninja import ModelSchema, Schema
 
 from django.contrib.auth import get_user_model
 
+from account_management.models import Ledger
+
 
 class UserSchema(Schema): 
     username: str 
@@ -12,3 +14,15 @@ class UserResponseSchema(ModelSchema):
     class Config: 
         model = get_user_model() 
         model_exclude = ['password', 'validation_token']
+
+class LedgerSchema(ModelSchema): 
+    class Config: 
+        model = Ledger 
+        model_fields = ['number', 'name', 'description'] 
+
+
+class LedgerResponseSchema(ModelSchema): 
+    class Config: 
+        model = Ledger 
+        model_exclude = ['user']
+    
