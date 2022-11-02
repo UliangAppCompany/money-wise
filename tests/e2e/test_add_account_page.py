1d240207f7a337f1c531b37e0b1f4da96335ab0a
Tang U-Liang <uliang6482@gmail.com>
2022-11-02 21:53:00 +0800

    ul-24 Deleted old e2e test module

--- tests/e2e/test_add_account_page.py
+++
@@ -1,7 +0,0 @@
-import pytest 
-
-@pytest.mark.django_db
-@pytest.mark.usefixtures("register_new_user", "validate_new_user") 
-def test_click_submit_and_continue_button_fires_api_post_request(driver): 
-    pass
-     
