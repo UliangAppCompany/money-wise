34fe0db3e20f4a46d4692cbcd48ab86a2be10f08
Tang U-Liang <uliang6482@gmail.com>
2022-11-02 08:24:01 +0800

    ul-24 Renamed test_add_account_page to tesT_login_page

--- tests/e2e_tests/test_login_page.py
+++ tests/e2e_tests/test_login_page.py
@@ -1,6 +1,6 @@
 import pytest 
 
-from django.test import override_settings
+from django.test import  override_settings
 from selenium.webdriver.common.by import By 
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions 
@@ -9,6 +9,7 @@ pytestmark = [
     pytest.mark.django_db, 
     pytest.mark.usefixtures("register_new_user", "validate_new_user") 
 ]
+
 
 @override_settings(DEBUG=True)
 def test_user_can_login_at_the_login_page(server, driver):
@@ -23,15 +24,8 @@ def test_user_can_login_at_the_login_page(server, driver):
     submit_button = driver.find_element(By.ID, "login-form-submit-button")
     submit_button.click()
 
-    elem = WebDriverWait(driver, 60).until(expected_conditions
+    elem = WebDriverWait(driver, 30).until(expected_conditions
         .text_to_be_present_in_element((By.ID, "auth-message"), "Authenticated!"))
 
     assert elem is not None
 
-
-
-def test_click_submit_and_continue_button_fires_api_post_request(driver): 
-    pass
-
-
-     
