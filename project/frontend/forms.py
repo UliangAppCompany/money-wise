7d617fcd1b388d0d479aa8d564861d5fa7690be5
Tang U-Liang <uliang6482@gmail.com>
2022-11-04 07:25:46 +0800

    ul-29 Updated forms.py
    Added Form for creating new password

--- project/frontend/forms.py
+++ project/frontend/forms.py
@@ -9,3 +9,7 @@ class AccountManagementAddAccountForm(forms.ModelForm):
 class AuthenticationForm(forms.Form): 
     username = forms.EmailField(required=True, max_length=100) 
     password = forms.CharField(widget=forms.PasswordInput(), max_length=100)
+
+class ChangePasswordForm(forms.Form): 
+    password = forms.CharField(widget=forms.PasswordInput(), max_length=100)
+    retype_password = forms.CharField(widget=forms.PasswordInput(), max_length=100)
