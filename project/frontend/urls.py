b26fd69b5f1f80e750d73ecaeec31e218de79558
uliang <uliang6482@gmail.com>
2022-11-04 10:43:57 +0800

    ul-29 Updated frontend urls.py
    Updated name of route to change password page.

--- project/frontend/urls.py
+++ project/frontend/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('ledger/<int:ledger_id>/account', views.add_account, name='add-account'), 
     path('ledger/<int:ledger_id>', views.ledger_page, name='ledger-page'), 
     path('login', views.login_page, name="login-page"),
-    path('user/<int:user_id>/change-password', views.change_password_page, name="change-password-page")
+    path('user/<int:user_id>/set-password', views.set_password_page, name="set-password-page")
 ]
