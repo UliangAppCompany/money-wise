a17847b4662095ccdeead9efd101fdab46586322
Tang U-Liang <uliang6482@gmail.com>
2022-11-09 22:57:59 +0800

    ul-30 Updated registration views.py
    Improved handling for user not found and user validate user function
    to perform validation.

--- project/registration/views.py
+++ project/registration/views.py
@@ -1,21 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import get_user_model 
 
+from .service import validate_user
 # Create your views here.
 
 def validate_registration(request): 
     token = request.GET['token'] 
     username = request.GET['username']
-    user = get_user_model().objects.get(username=username) 
+    user  = get_object_or_404(get_user_model(), username=username)
     # breakpoint()
     if user.token_is_valid(token): 
-        user.is_validated = True
-        user.save() 
-
-    return HttpResponseRedirect(reverse('set-new-password')) 
+        validate_user(user)
+    return HttpResponseRedirect(reverse('set-new-password',args= [ user.id ])) 
 
 
-def set_new_password(request): 
+def set_new_password(request, user_id): 
     ...
