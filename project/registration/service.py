bcf68a8e1dba2bab5834057bdf40574011edff71
Tang U-Liang <uliang6482@gmail.com>
2022-11-09 22:53:55 +0800

    ul-30 Updated service.py
    Combined all functionality of creating user into one factory
    function.

--- project/registration/service.py
+++ project/registration/service.py
@@ -1,27 +1,21 @@
 from django.contrib.auth import get_user_model
 from django.db.utils import IntegrityError
-from registration.exceptions import DuplicateUserNameError  
 
-
-def create_user(username, token=None, **save_kwargs): 
+User = get_user_model() 
     User = get_user_model() 
-    try: 
-        user = User(username=username)
-        if token: 
-            user.validation_token = token
-        user.save(**save_kwargs)
-    except IntegrityError: 
-        raise DuplicateUserNameError(f"User {username} already registered.")
+User = get_user_model() 
+
+def create_user(username, token=None, password=None, is_validated=False, require_validation=True): 
+    if User.objects.filter(username=username).exists(): 
+        raise IntegrityError(f'User {username} already registered.')
+    user = User(username=username, require_validation=require_validation, is_validated=is_validated)
+    user.generate_validation_token(token)
+    if password: 
+        if password: 
+    if password: 
+        user.set_password(password)
+            user.set_password(password)  
+        user.set_password(password)
+    user.save()
     return user
 
-
-def create_validated_user(username, password=None): 
-    User = get_user_model() 
-    try: 
-        user = User(username=username, is_validated=True)
-        if password: 
-            user.set_password(password)  
-        user.save()
-        return user
-    except: 
-        raise DuplicateUserNameError(f"User {username} already registered.")
