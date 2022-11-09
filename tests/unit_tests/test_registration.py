34ca63b772bc8fdc9ffce47ff61360251693026e
Tang U-Liang <uliang6482@gmail.com>
2022-11-09 22:51:19 +0800

    ul-30 Added test_registration.py
    Added test suite for registration application. This includes tests
    for service module, validation email sent and validation route.

---
+++ tests/unit_tests/test_registration.py
@@ -0,0 +1,107 @@
+import time
+from datetime import timedelta
+
+import pytest 
+
+from django.core import mail 
+from django.test import override_settings
+from django.db.utils import IntegrityError
+
+from registration.exceptions import TokenExpiredError, UnvalidatedUserError
+from registration.service import create_user, get_user
+
+pytestmark = [
+    pytest.mark.django_db, 
+]
+
+class TestCreateUserFunction: 
+    def test_create_new_user_service_creates_new_user_if_not_present(self, cursor): 
+        create_user('john@example.com', require_validation=False)
+        cursor.execute("select username from registration_user ") 
+        result = cursor.fetchone()
+        assert result == ('john@example.com', )  
+
+    def test_that_new_user_cannot_be_created_if_already_present(self, ): 
+        create_user('john@example.com', require_validation=False)
+        with pytest.raises(IntegrityError) as exc: 
+            create_user('john@example.com', require_validation=False)
+        
+            assert str(exc) == "User john@example.com already registered."
+
+    def test_that_unvalidated_user_cannot_set_password(self, john): 
+        with pytest.raises(UnvalidatedUserError) as exc: 
+            john.set_password('password') 
+            assert str(exc) == "Password cannot be set on unvalidated users."
+    
+    def test_that_password_can_be_set_on_users_that_do_not_require_validation(self): 
+        john = create_user('john@example.com', require_validation=False, is_validated=False) 
+        try: 
+            john.set_password('password') 
+        except UnvalidatedUserError: 
+            pytest.fail('Error should not be raised.')
+    
+    def test_that_custom_token_can_be_set(self, cursor): 
+        john = create_user('john@example.com', token='abc', require_validation=True)
+
+        cursor.execute('select validation_token from registration_user')
+        result = cursor.fetchone() 
+
+        assert result == ('abc', )
+
+
+class TestUserValidationEmail: 
+    def test_that_validation_email_is_sent_when_new_user_is_successfully_saved_in_db(self, john): 
+        assert len(mail.outbox) == 1    
+        message = mail.outbox[0] 
+
+        assert message.subject == "New user validation link" 
+        assert message.from_email == "admin@money-wise.com.my"
+        assert message.to == ["john@example.com"]
+        assert f'registration/validate?username=john@example.com&token={john.validation_token}' in message.body 
+
+    def test_that_email_is_not_sent_when_user_does_not_require_validation(self): 
+        john = create_user('john@example.com', require_validation=False, is_validated=False)
+        assert len(mail.outbox) == 0
+
+    def test_that_email_is_not_sent_when_user_info_is_updated(self, john): 
+        mail.outbox = [] 
+        john.first_name = 'John'
+        john.save()
+        assert len(mail.outbox) == 0 
+
+@pytest.fixture 
+def set_validation_token(john): 
+    john.is_validated= False 
+    john.validation_token = 'abc' 
+    john.save()
+
+@pytest.mark.usefixtures('set_validation_token')
+class TestValidationRoute: 
+    def test_validation_url(self,client): 
+        response = client.get('/registration/validate?username=john@example.com&token=abc')
+        assert response.status_code == 302
+
+    def test_that_clicked_validation_link_validates_user(self,client, ): 
+        response =client.get('/registration/validate?username=john@example.com&token=abc')
+        john = get_user('john@example.com')
+        assert john.is_validated 
+
+    @override_settings(
+        VALIDATION_TOKEN_EXPIRY = timedelta(seconds=0.5)
+    )
+    def test_that_user_cannot_be_validated_if_validation_link_has_expired(self, client): 
+        time.sleep(0.6) 
+        with pytest.raises(TokenExpiredError):
+            response = client.get('/registration/validate?username=john@example.com&token=abc')
+        john = get_user('john@example.com')
+        assert not john.is_validated
+
+    def test_that_if_token_is_incorrect_user_is_not_validated(self, client,): 
+        response = client.get('/registration/validate?username=john@example.com&token=asafsiudf')
+        john = get_user('john@example.com')
+        assert not john.is_validated
+
+    def test_that_if_user_not_found_response_is_404(self, client): 
+        response = client.get('/registration/validate?username=gary@example.com&token=abc')
+        assert response.status_code == 404
+
