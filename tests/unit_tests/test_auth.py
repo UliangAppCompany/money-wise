5734d40462e56cc1ce2b57e67999bfe501afcb7d
Tang U-Liang <uliang6482@gmail.com>
2022-11-10 00:01:04 +0800

    ul-30 Updated test_auth.py
    Refactored tests into classes. One for testing the api endpoint for
    authentication and another for the frontend page.

--- tests/unit_tests/test_auth.py
+++ tests/unit_tests/test_auth.py
@@ -3,33 +3,45 @@ import pytest
 from django.contrib.auth import get_user
 from django.test import RequestFactory
 
+from frontend.forms import AuthenticationForm
+from registration.service import create_user
+
+
 pytestmark =  [
     pytest.mark.django_db,
-    pytest.mark.usefixtures(
-        "register_new_user", 
-        "validate_new_user"
-    )]
+]
+
+@pytest.fixture 
+def register_john(): 
+    create_user('john@example.com', password='password', require_validation=False)
 
 @pytest.fixture 
 def login_response(client): 
-    response = client.post('/api/login', data={'username': 'john@example.com', 
+    response = client.post('/api/auth/login', data={'username': 'john@example.com', 
         'password': 'password'},  
         content_type = 'application/json')
     return response
 
+@pytest.mark.usefixtures('register_john')
+class TestLoginApiEndpoint: 
+    def test_login_api_enpoint_authenticates_user(self,login_response): 
+        assert login_response.status_code == 200 
 
-def test_login_api_enpoint_authenticates_user(login_response, client): 
-    assert login_response.status_code == 200 
+    @pytest.mark.usefixtures('register_john', 'login_response')
+    def test_subsequent_request_contain_authenticated_user_session_id(self, client): 
+        factory = RequestFactory() 
+        request = factory.get('/') 
+        request.session = client.session 
 
-    factory = RequestFactory() 
-    request = factory.get('/') 
-    request.session = client.session 
+        user = get_user(request)
+        assert user.username == 'john@example.com' 
 
-    user = get_user(request)
-    assert user.username == 'john@example.com' 
+class TestLoginPage: 
+    def test_login_page_loads(self,client): 
+        response = client.get('/login') 
+        assert response.status_code == 200 
 
-def test_login_page_loads(client): 
-    response = client.get('/login') 
-
-    assert response.status_code == 200 
+    def test_login_page_loads_correct_form(self, client): 
+        response = client.get('/login')
+        assert isinstance(response.context['form'], AuthenticationForm)
 
