91d8df7185687d1df4ce33234e359a5b387bafb1
Tang U-Liang <uliang6482@gmail.com>
2022-11-09 22:59:25 +0800

    ul-30 Updated registration service.py
    Added get_user convenience function

--- project/registration/service.py
+++ project/registration/service.py
@@ -2,8 +2,6 @@ from django.contrib.auth import get_user_model
 from django.db.utils import IntegrityError
 
 User = get_user_model() 
-    User = get_user_model() 
-User = get_user_model() 
 
 def create_user(username, token=None, password=None, is_validated=False, require_validation=True): 
     if User.objects.filter(username=username).exists(): 
@@ -11,10 +9,6 @@ def create_user(username, token=None, password=None, is_validated=False, require
     user = User(username=username, require_validation=require_validation, is_validated=is_validated)
     user.generate_validation_token(token)
     if password: 
-        if password: 
-    if password: 
-        user.set_password(password)
-            user.set_password(password)  
         user.set_password(password)
     user.save()
     return user
@@ -24,4 +18,8 @@ def validate_user(user:User) -> User :
         user.is_validated= True
         user.save()
     return user 
+
+def get_user(username:str) -> User: 
+    return User.objects.get(username=username) 
+
 
