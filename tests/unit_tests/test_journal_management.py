24b60d8be9ca223abac328aa2989c2adbb838189
Tang U-Liang <uliang6482@gmail.com>
2022-11-16 07:29:43 +0800

    test_journal_management refactoring

--- tests/unit_tests/test_journal_management.py
+++ tests/unit_tests/test_journal_management.py
@@ -1,6 +1,13 @@
+from decimal import Decimal
+import datetime 
 import pytest 
+from pytz import timezone 
 
+from django.conf import settings
 from django.test import override_settings
+
+from account_management.models import Journal
+
 
 pytestmark = [
     pytest.mark.django_db 
@@ -16,7 +23,7 @@ class TestJournalModel:
         assert result == [(1, 'Company A Journal')]
 
     def test_journal_create_double_entry(self, cursor, journal): 
-        cursor.execute("select notes from account_management_entry " 
+        cursor.execute("select note from account_management_entry " 
                 "where journal_id = %s", 
                 [journal.id])
         result = cursor.fetchall() 
@@ -35,9 +42,8 @@ class TestJournalModel:
         result = cursor.fetchall()
 
         assert result == [
-                (100, 1050, 0, "to 100-Cash account"), 
-                (300, 0, 1050, "from 300-Revenue account")
-                ]
+                (100, Decimal('1050.00'), Decimal('0.00'), "from 300-Revenue account"), 
+                (300, Decimal('0.00'), Decimal('1050.00'), "to 100-Cash account")]
 
     @pytest.mark.parametrize('account_num,expected', [
         (100, ("being collections from cash register", 1050, 0, 1050, 0)), 
@@ -51,16 +57,46 @@ class TestJournalModel:
                 "where number = %s "
                 "and ledger_id = %s ", [account_num, ledger.id] )
         result = cursor.fetchall()
-
         assert result == [expected]
 
-@pytest.mark.usefixtures('init_john', 'login_john')
+    def execute_sql(self, cursor, stmt): 
+        cursor.execute(stmt) 
+        return cursor.fetchall()
+    
+    # @override_settings(DEBUG=True)
+    def test_that_post_endpoint_responds(self,url,payload,stmt,db_expected, client):
+        response = self.post_response(client, url, payload)
+        assert response.status_code == 200
+
+    def test_that_unauthenticated_user_cannot_create_journals(self,url,payload,stmt,db_expected, client): 
+        client.logout()
+        response = self.post_response(client, url, payload)
+        assert response.status_code == 401
+
+    def test_that_post_endpoint_adds_resource_to_db(self,url,payload,stmt,db_expected, client, cursor):
+        self.post_response(client, url, payload) 
+        result = self.execute_sql(cursor, stmt)
+
+        assert result == db_expected
+
+
+@pytest.mark.usefixtures('add_accounts_to_ledger', 'init_john', 'login_john')
+@pytest.mark.parametrize('url,payload,stmt,db_expected', [
+    ('/api/account-management/journal', {
+        'number': 1, 
+        'name': 'Company A Journal', 
+        'description': 'description' 
+    }, 
+    """select name from account_management_journal where 
+    number = 1 and account_management_journal.user_id = ( 
+    select id from registration_user where username = 'john@example.com' )""", 
+        [('Company A Journal', )]), 
+])
 class TestJournalApiEndpoint: 
     def post_response(self, client, url, payload): 
         response = client.post(url, data=payload, 
             content_type='application/json' )
         return response
-
     # @override_settings(DEBUG=True)
     @pytest.mark.parametrize('url,payload', [
         ('/api/account-management/journal', {
